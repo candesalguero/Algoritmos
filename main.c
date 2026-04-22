1d81c057423170e003522cb5f35e43e9be330c4f
Dasha-16 <apollarodasha16@gmail.com>
2026-04-22 12:33:19 -0300

    base del proyecto, elimine pila

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
     tConfiguracion config;
     if (!leerConfig(&config))
     {
-        return 1;
+        return 1; // Terminamos si no hay archivo de configuración
     }
 
     srand((unsigned int)time(NULL)); //srand le pasa la hora actual como punto de arranque para que el resultado del dado (rand) no siga siempre la misma secuencia
